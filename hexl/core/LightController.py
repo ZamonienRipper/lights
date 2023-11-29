e2883b16c66757141302542c275f6c85b31bd10a
Jonathan McGowan <mcgowan110@hotmail.com>
2023-11-29 21:43:12 +0100

    first implementation of gui lights

--- hexl/core/LightController.py
+++ hexl/core/LightController.py
@@ -1,5 +1,6 @@
 import time
 from rpi_ws281x import PixelStrip, Color
+from hexl.core.LightControllerParent import LightControllerParent
 
 # LED strip configuration:
 LED_COUNT = 144        # Number of LED pixels.
@@ -22,7 +23,7 @@ LED_INDEXES_OUTER = [6, 7, 8, 9, 10, 11]
 LED_INDEXES_INNER = [0, 1, 2, 3, 4, 5]
 wait_ms = 50
 
-class LightController:
+class LightController(LightControllerParent):
     def col(self, color):
         """Returns strip-readable color code from RGB components"""
         #print(f"color0: {color[0]}")
@@ -30,95 +31,13 @@ class LightController:
         #print(f"color2: {color[2]}")
         return Color(color[0], color[1], color[2])
 
-    def pixelList(self, band):
-        """Create list of all pixels in specified band"""
-        targets = self.bandsDict[band]
-        pixels = []
-        for target in targets:
-            pixels.append(LED_INDEXES[target])
-        pixelList = [item for sublist in pixels for item in sublist]
-        return pixelList
-
     def __init__(self):
         self.strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
         self.strip.begin()
         self.bandsDict = {'outer': LED_INDEXES_OUTER, 'inner': LED_INDEXES_INNER}
         self.pixelsDict = {'outer': self.pixelList('outer'), 'inner': self.pixelList('inner')}
+        self.LED_INDEXES = LED_INDEXES
         print(f"bandsDict: {self.bandsDict}")
         print(f"pixelsDict: {self.pixelsDict}")
 
-    def pixelChange(self, pixel, color):
-        """Cahnge color of one specified pixel"""
-        for led in LED_INDEXES[pixel]:
-            self.strip.setPixelColor(led, self.col(color))
-        self.strip.show()
-
-    def pixelsChange(self, pixels, colors):
-        """Change color of specified pixels"""
-        #print(f"pixels: {pixels}")
-        #print(f"colors: {colors}")
-        for i, pixel in enumerate(pixels):
-            for led in LED_INDEXES[pixel]:
-                self.strip.setPixelColor(led, self.col(colors[i]))
-        self.strip.show()
-
-    def bandChange(self, band, pixels, colors, wipeFirst=True):
-        """Change color of all pixels in specified band"""
-        targets = self.bandsDict[band]
-        if wipeFirst:
-            self.bandOff(band, active=False)
-        for i, pixel in enumerate(pixels):
-            for led in LED_INDEXES[targets[pixel]]:
-                self.strip.setPixelColor(led, self.col(colors[i]))
-        self.strip.show()
-
-    def pixelOff(self, pixel, active=True):
-        """Turn off specified pixel"""
-        for led in LED_INDEXES[pixel]:
-            self.strip.setPixelColor(led, Color(0, 0, 0))
-        if active:
-            self.strip.show()
-
-    def bandOff(self, band, active=True):
-        """Turn off all pixels in specified band"""
-        targets = self.pixelsDict[band]
-        for led in targets:
-            self.strip.setPixelColor(led, Color(0, 0, 0))
-        if active:
-            self.strip.show()
-
-    def colorWipe(self, color, wait_ms=50):
-        """Wipe color across display a LED at a time."""
-        for i in range(self.strip.numPixels()):
-            self.strip.setPixelColor(i, self.col(color))
-            self.strip.show()
-            time.sleep(wait_ms / 1000.0)
-
-    def colorWipeFast(self, color, wait_ms=150):
-        """Wipe color across display a pixel at a time"""
-        for pixel in LED_INDEXES:
-            print(f"Turing on LEDs {pixel}")
-            for led in pixel:
-                self.strip.setPixelColor(led, self.col(color))
-            self.strip.show()
-            time.sleep(wait_ms / 1000.0)
-            
-    def offWipeFast(self, wait_ms=150):
-        """Turn off display a pixel at a time"""
-        for i, indeces in enumerate(LED_INDEXES):
-            print(f"Turing off pixel {i}, which contains leds {indeces}")
-            self.pixelOff(i)
-            time.sleep(wait_ms / 1000.0)
-
-    def celebrate(self):
-        """Wipe festive colors across display a pixel at a time"""
-        celebrationColors = ((0,255,0),(0,0,255),(0,255,0),(0,0,255),(0,0,0))
-        for color in celebrationColors:
-            self.colorWipeFast(color)
-
-    def death(self):
-        """Wipe ominous colors across display a pixel at a time"""
-        deathColors = ((255,0,0),(0,0,0),(255,0,0),(0,0,0))
-        for color in deathColors:
-            self.colorWipeFast(color)
 
