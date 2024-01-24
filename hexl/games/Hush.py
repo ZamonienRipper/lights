ee3240d4b1f0b03c45158a141c644c8531041fc8
ZamonienRipper <mcgowan110@hotmail.com>
2024-01-24 20:32:47 +0100

    Added: player select and winner screen for Hush

--- hexl/games/Hush.py
+++ hexl/games/Hush.py
@@ -10,17 +10,20 @@ class Hush:
         self.colorful = False
         self.points = 0
         self.suits = 2
+        self.game_points = 3
         self.gaming = True
         self.fields = [0,1,2,3,4,5,6,7,8,9,10,11,12]
         self.grid = {12:0, 0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0}
         self.colors = [(0,0,0), (0,255,0), (0,0,255), (255,0,255)]
         self.inner = deque([2,1,0,5,4,3], maxlen=6)
-        self.randomization_times = [.5,.5,.4,.4,.3,.3,.2,.2,.1,.1,.1,.1,.1,.1,.1]
+        self.randomization_times = [.5,.5,.4,.4,.3,.3,.2,.2,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.3,.5]
         #self.randomization_times = [.1,.1]
-        self.single_player = [(0,0,0),(0,0,0),(255,255,255)]
-        self.two_player = [(0,0,0),(255,255,0),(255,255,255)]
-        self.three_player = [(0,255,255),(255,255,0),(255,255,255)]
-        self.players = [self.single_player, self.two_player, self.three_player]
+        self.single_player = deque([1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        self.two_player = deque([1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        self.three_player = deque([1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        self.player_indicators = [self.single_player, self.two_player, self.three_player]
+        self.player_seq = [deque([1]), deque([1,2]), deque([1, 2, 3])]
+        self.players = 1
 
     def nxt(self, field):
         if self.inner.index(field) in [0,5]:
@@ -85,6 +88,7 @@ class Hush:
 
     def getInput(self, gameFreq, LightController, eventPile):
         colorWheel = ((0,255,0), (127,255,0), (255,255,0), (255,127,0), (255,0,0))
+        player_indicator = self.player_indicators[self.player_seq[0]-1].copy()
         eventPile.clear()
         wait = 1
         while wait:
@@ -96,6 +100,9 @@ class Hush:
                 time.sleep(0.4)
                 while wait:
                     LightController.pixelsChange([self.out(self.nxt(direction)), self.out(self.prv(direction))], [(255,255,255), (255,255,255)])
+                    indicator = player_indicator[0]
+                    LightController.pixelChange(direction, (indicator*255,indicator*255,indicator*255))
+                    player_indicator.rotate(-1)
                     if eventPile:
                         clockwise = np.where(eventPile.popleft() == 1)[0][0]
                         print(f'Chosen clockwise: {clockwise}')
@@ -149,51 +156,96 @@ class Hush:
             self.grid[winner] = 0
         print(f'{winners=}')
         for i in range(3):
-            
             LightController.pixelsChange(winners, [self.colors[winning_color] for x in winners])
             time.sleep(1)
             LightController.pixelsChange(winners, [(0,0,0) for x in winners])
             time.sleep(.5)
 
     def selectMode(self, LightController, eventPile):
-        player_selectors = {1:6, 2:7, 3:8}
+        player_selectors = {1:7, 2:8, 3:9}
         
-        lights_sequence = deque([6, 0, 7, 7, 0, 8, 8, 8, 0, ])
+        lights_sequence = deque([7, 0, 8, 8, 0, 9, 9, 9, 0])
         
         while 1:
             if lights_sequence[0] != 0:
                 LightController.pixelChange(lights_sequence[0], (255,255,255))
-            time.sleep(0.25)
-            LightController.pixelChange(lights_sequence[0], (0,0,0))
-            time.sleep(0.2)
+                time.sleep(0.2)
+                LightController.pixelChange(lights_sequence[0], (0,0,0))
+                time.sleep(0.2)
+            else:
+                time.sleep(0.2)
             lights_sequence.rotate(-1)
             if eventPile:
                 players = np.where(eventPile.popleft() == 1)[0][0]
                 eventPile.clear()
                 if players in player_selectors.keys():
                     for i in range(players):
-                        LightController.pixelChange(12, [(255,255,255)])
+                        LightController.pixelChange(12, (255,255,255))
                         time.sleep(0.3) 
-                        LightController.pixelChange(12, [(0,0,0)])
-                    self.players = self.players[players] 
+                        LightController.pixelChange(12, (0,0,0))
+                        time.sleep(0.3) 
+                    self.players = players
+                    self.player_seq = self.player_seq[players-1]
+                    self.score = np.zeros(players)
+                    print(f'Player selection done: {self.players}')
                     break
                 else:
                     continue
 
-        
+    def showScore(self, gameFreq, LightController, eventPile):
+        player_one_seq = deque([1, 1, 1, 0, 0, 1, 1, 1])
+        player_two_seq = deque([1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1])
+        player_three_seq = deque([1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1])
+        player_seqs = [player_one_seq, player_two_seq, player_three_seq]
+        eventPile.clear()
+        game_over = 0
+        score_pixels = [0, 2, 4]
+        score_colors = [(0, 0, 0), (0, 0, 0), (0, 0, 0)]
+        winning_score = max(self.score)
+        for player, score in enumerate(self.score):
+            if score == winning_score:
+                score_colors[player] = (0,255,0)
+            else:
+                score_colors[player] = (255,255,255)
+        time.sleep(0.4)
+        print(f'{self.score=}')
+        print(f'{score_colors=}')
+        while 1:
+            for i in range(self.players):
+                for j in range(8+6*i):
+                    LightController.pixelChange(score_pixels[i], tuple(k*player_seqs[i][0] for k in score_colors[i]))
+                    player_seqs[i].rotate(-1)
+                    time.sleep(gameFreq)
+                    if eventPile:
+                        print('GAME OVER')
+                        print(f'{eventPile=}')
+                        LightController.offWipeFast(50)
+                        game_over = 1
+                        break
+                if game_over:
+                    break
+            if game_over:
+                break
         
     def play(self, gameFreq, LightController, eventPile):
         self.selectMode(LightController, eventPile)
         self.randomize_grid(LightController)
-        while self.points < 3:
+        while sum(self.score) < self.game_points:
             
             (dir, clockwise) = self.getInput(gameFreq, LightController, eventPile)
             self.rotate_grid(dir, clockwise)
             self.update_light(LightController)
+            self.player_seq.rotate(-1)
             winner = self.checkGrid()
             print(winner)
             if winner != None:
-                self.points += 1
+                self.score[self.player_seq[0]-1] += 1
                 self.removeWinner(winner, LightController)                   
             self.update_light(LightController)
+        LightController.offWipeFast(50)
+        self.score = [1, 0, 1]
+        if self.players > 1:
+            self.showScore(gameFreq, LightController, eventPile)
+        print('GAME OVER')
+        
 
